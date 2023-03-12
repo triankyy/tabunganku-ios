5e5f187de403c3c626bdbc0d2779b18a201c854d
triankyy <triankyy@gmail.com>
2023-03-13 01:27:47 +0700

    Initial Commit

---
+++ Tabunganku/TabungankuApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabungankuApp.swift
+//  Tabunganku
+//
+//  Created by Kyy on 3/13/23.
+//
+
+import SwiftUI
+
+@main
+struct TabungankuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
