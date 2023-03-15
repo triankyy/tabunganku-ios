414431c934d90564637de791327dd1c5b9a6912b
triankyy <triankyy@gmail.com>
2023-03-16 00:58:04 +0700

    Add Home Screen UI

--- Tabunganku/ContentView.swift
+++ Tabunganku/ContentView.swift
@@ -9,13 +9,57 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            ScrollView {
+                VStack(alignment: .leading, spacing: 10) {
+                    Divider()
+                    let color: Array<Color> = [.blue, .red, .yellow, .cyan, .green, .orange, .indigo, .teal, .purple]
+                    ForEach(0..<10) { item in
+                        HStack(spacing: 20) {
+                            if let randomColor = color.randomElement() {
+                                RoundedRectangle(cornerRadius: 50, style: .circular)
+                                    .fill(randomColor)
+                                    .frame(width: 60, height: 60)
+                                    .overlay {
+                                        Text("T")
+                                            .foregroundColor(.white)
+                                    }
+                            }
+                            VStack(alignment: .leading, spacing: 6) {
+                                Text("Tabunganku")
+                                    .font(.subheadline)
+                                    .bold()
+                                    .lineLimit(1)
+                                
+                                Text("Rp. 200.000")
+                                    .font(.footnote)
+                                    .foregroundColor(.secondary)
+                                    .lineLimit(1)
+                                
+                                Divider()
+                                    .padding([.top])
+                            }
+                        }
+                        .padding([.top, .bottom], 8)
+                    }
+                }
+                .padding()
+                .frame(maxWidth: .infinity)
+            }
+            .navigationTitle("Tabunganku")
+            .toolbar {
+                ToolbarItem(placement: .navigation) {
+                    Button(action: {}) {
+                        Text("Edit")
+                    }
+                }
+                ToolbarItem {
+                    Button(action: {}) {
+                        Image(systemName: "plus")
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
